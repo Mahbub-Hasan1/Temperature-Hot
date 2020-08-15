d1bfa222b84b29142a309ee13fd377d6d3d17cd6
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-08-15 16:41:28 +0600

    first commit

--- mine.js
+++ mine.js
@@ -8,16 +8,27 @@ button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputBox.value+'&appid=53f6bf57d12e08dc2f9ebd6b92958edb')
     .then(Response => Response.json())
     .then(data => {
-        var nameNumber = data['sys']['country'];
-        var degNumber = data['wind']['deg'];
+        var nameNumber = data['name'];
+        var degNumber = data['main']['temp'];
         var cloudNumber = data['weather'][0]['main'];
 
+        // degNumber = degNumber / 10;
+        // degNumber.toFixed(1);
+
         document.querySelector('h1').innerHTML = nameNumber;
-        desc.innerHTML = degNumber;
+        desc.innerHTML = (degNumber / 10).toFixed(1);
         cloud.innerText = cloudNumber;
+
+        
+
         console.log(data);
         // console.log(cloudNumber)
+        
     })
 
     .catch(err => alert('Wrong city name!'));
+
 })
+// if (cloudNumber.innerHTML == Haze) {
+//     document.getElementById('img').style.backgroundImage = url('1.jpg');
+// }
